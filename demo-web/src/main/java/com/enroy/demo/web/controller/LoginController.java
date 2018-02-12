3c0fff1c2cb5a52726659b188c63e918f861a24f
zhuchao <zhuchao@hd123.com>
2018-02-12 10:54:20 +0800

    用户登录

--- demo-web/src/main/java/com/enroy/demo/web/controller/LoginController.java
+++ demo-web/src/main/java/com/enroy/demo/web/controller/LoginController.java
@@ -20,6 +20,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -40,8 +41,8 @@ public class LoginController extends BaseController {
   @Autowired
   LoginService loginService;
 
-  @RequestMapping(path = "login.hd", method = RequestMethod.POST)
-  public ActionResult login(User user, HttpServletRequest request, HttpServletResponse response,
+  @RequestMapping(path = "login.zc", method = RequestMethod.POST)
+  public ActionResult login(@RequestBody User user, HttpServletRequest request, HttpServletResponse response,
                             ModelMap model) throws Exception {
 
     HttpSession session = request.getSession();
@@ -59,7 +60,7 @@ public class LoginController extends BaseController {
       return ActionResult.fail(loginResult.getMessage());
     }
 
-    return null;
+    return ActionResult.ok();
   }
 
 }
